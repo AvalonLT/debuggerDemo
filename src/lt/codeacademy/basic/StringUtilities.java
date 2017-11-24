91b72821fb219abad5e9caae3cee0abfd9b96a27
AvalonLT <30309451+AvalonLT@users.noreply.github.com>
2017-11-24 11:54:54 +0200

    Initial commit

---
+++ src/lt/codeacademy/basic/StringUtilities.java
@@ -0,0 +1,11 @@
+package lt.codeacademy.basic;
+
+public class StringUtilities {
+    private StringBuilder stringBuilder = new StringBuilder();
+    private int charsAdded = 0;
+
+    public void addChar(StringBuilder stringBuilder, char c) {
+        stringBuilder.append(c);
+        charsAdded++;
+    }
+}
